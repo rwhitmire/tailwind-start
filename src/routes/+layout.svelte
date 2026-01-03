642116063a6ebc02fe30a441b3ddbbb59b2eba3c
Ryan Whitmire <ryanw51@gmail.com>
2026-01-03 11:40:51 -0500

    add logo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import "./layout.css";
   import favicon from "$lib/assets/favicon.svg";
+  import logo from "$lib/assets/logo.svg";
   import ThemeToggle from "$lib/components/ThemeToggle.svelte";
 
   let { children } = $props();
@@ -10,7 +11,10 @@
 
 <div class="sticky top-0 z-10 flex items-center bg-(--body-bg) px-6 py-3">
   <div class="flex-1">
-    <a href="/">Tailwind Start</a>
+    <a href="/" class="flex items-center gap-2 font-semibold text-lg hover:opacity-80 transition-opacity">
+      <img src={logo} alt="Tailwind Start" class="h-8 w-8" />
+      <span>Tailwind Start</span>
+    </a>
   </div>
   <div class="flex flex-5 justify-center">
     <a href="/docs" class="rounded-md px-3 py-2 hover:bg-neutral-100 dark:hover:bg-neutral-900">
